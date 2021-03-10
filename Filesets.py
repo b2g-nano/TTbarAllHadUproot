9da88dbbbd35a9676cdbfdd65343bc84e022fbe4
acwillia <acwillia@buffalo.edu>
2021-03-10 12:15:27 -0600

    New Physics UL16 MC (with JEC)

--- Filesets.py
+++ Filesets.py
@@ -37,7 +37,7 @@ ttbarfilename = 'TTbarAllHadUproot/TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8.txt
 with open(ttbarfilename) as f:
     ttbarfiles = [xrootdstr2 + s.strip() for s in f.readlines()]
 
-ZprimeDMfilename = 'TTbarAllHadUproot/ZprimeDMToTTbar.txt'
+ZprimeDMfilename = 'TTbarAllHadUproot/ZprimeDMToTTbar_UL16.txt'
 #ZprimeDMfilename = 'ZprimeDMToTTbar.txt'
 with open(ZprimeDMfilename) as f:
     DM1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp1000" in s]
@@ -58,15 +58,36 @@ with open(ZprimeDMfilename) as f:
 with open(ZprimeDMfilename) as f:
     DM5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp5000" in s]
 
+RSGluonfilename = 'TTbarAllHadUproot/RSGluonToTT.txt'
+#RSGluonfilename = 'RSGluonToTT.txt'
+with open(RSGluonfilename) as f:
+    RSGluon1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1000" in s]
+with open(RSGluonfilename) as f:
+    RSGluon1500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1500" in s]
+with open(RSGluonfilename) as f:
+    RSGluon2000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2000" in s]
+with open(RSGluonfilename) as f:
+    RSGluon2500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2500" in s]
+with open(RSGluonfilename) as f:
+    RSGluon3000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3000" in s]
+with open(RSGluonfilename) as f:
+    RSGluon3500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3500" in s]
+with open(RSGluonfilename) as f:
+    RSGluon4000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4000" in s]
+with open(RSGluonfilename) as f:
+    RSGluon4500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4500" in s]
+with open(RSGluonfilename) as f:
+    RSGluon5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-5000" in s]
+
 jetdatafilename = 'TTbarAllHadUproot/JetHT_Data.txt'
 with open(jetdatafilename) as f:
     jetdatafiles = [xrootdstr2 + s.strip() for s in f.readlines()[::3]] # Every third datafile
 with open(jetdatafilename) as g:
-    jetdatafiles2016 = [xrootdstr2 + s.strip() for s in g.readlines() if "/store/data/Run2016" in s]
+    jetdatafiles2016 = [xrootdstr2 + s.strip() for s in g.readlines()[::3] if "/store/data/Run2016" in s]
 with open(jetdatafilename) as h:
-    jetdatafiles2017 = [xrootdstr2 + s.strip() for s in h.readlines() if "/store/data/Run2017" in s]
+    jetdatafiles2017 = [xrootdstr2 + s.strip() for s in h.readlines()[::3] if "/store/data/Run2017" in s]
 with open(jetdatafilename) as i:
-    jetdatafiles2018 = [xrootdstr2 + s.strip() for s in i.readlines() if "/store/data/Run2018" in s]
+    jetdatafiles2018 = [xrootdstr2 + s.strip() for s in i.readlines()[::3] if "/store/data/Run2018" in s]
 
 """ Comment out whichever files you wish to not be included """
 
@@ -81,6 +102,15 @@ filesets = {
     'DM4000':DM4000files,
     'DM4500':DM4500files,
     'DM5000':DM5000files,
+    'RSGluon1000':RSGluon1000files,
+    'RSGluon1500':RSGluon1500files,
+    'RSGluon2000':RSGluon2000files,
+    'RSGluon2500':RSGluon2500files,
+    'RSGluon3000':RSGluon3000files,
+    'RSGluon3500':RSGluon3500files,
+    'RSGluon4000':RSGluon4000files,
+    'RSGluon4500':RSGluon4500files,
+    'RSGluon5000':RSGluon5000files,
     'TTbar':ttbarfiles,
     'JetHT':jetdatafiles,
     'JetHT2016_Data':jetdatafiles2016,
@@ -99,6 +129,15 @@ filesets_forweights = {
     'DM4000':DM4000files,
     'DM4500':DM4500files,
     'DM5000':DM5000files,
+    'RSGluon1000':RSGluon1000files,
+    'RSGluon1500':RSGluon1500files,
+    'RSGluon2000':RSGluon2000files,
+    'RSGluon2500':RSGluon2500files,
+    'RSGluon3000':RSGluon3000files,
+    'RSGluon3500':RSGluon3500files,
+    'RSGluon4000':RSGluon4000files,
+    'RSGluon4500':RSGluon4500files,
+    'RSGluon5000':RSGluon5000files,
     'TTbar':ttbarfiles,
     'TTbar_2016':ttbarfiles, # ttbarfiles to be subtracted from JetHT2016 data
     'TTbar_2017':ttbarfiles, # ttbarfiles to be subtracted from JetHT2016 data
@@ -108,8 +147,4 @@ filesets_forweights = {
     'JetHT2017_Data':jetdatafiles2017,
     'JetHT2018_Data':jetdatafiles2018
 }
-
-
-
-
 
