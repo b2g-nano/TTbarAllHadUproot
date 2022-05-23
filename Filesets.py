a6fdf3b53c8473c01856b80eb6b3e3a017815bca
acwillia <acwillia@buffalo.edu>
2022-05-23 16:30:10 -0500

    Read UL16 files; No longer need to comment out unwanted datasets

--- Filesets.py
+++ Filesets.py
@@ -32,60 +32,61 @@ xrootdstr2 = 'root://cmsxrootd.fnal.gov/'
 xrootdstr3 = 'root://cmsxrootd-site.fnal.gov/'
 
 filedir = 'TTbarAllHadUproot/nanoAODv9Files/'
+VFP = '_postVFP.txt'
 
-qcdfilename = filedir + 'QCD/QCD_NanoAODv9_UL16_postVFP.txt'
-with open(qcdfilename) as f:
-    qcdfiles = [xrootdstr2 + s.strip() for s in f.readlines()]
+ul16qcdfilename = filedir + 'QCD/QCD_NanoAODv9_UL16' + VFP
+with open(ul16qcdfilename) as f:
+    ul16qcdfiles = [xrootdstr2 + s.strip() for s in f.readlines()]
 
-ttbar700to1000filename = filedir + 'TT/TT_Mtt-700to1000_NanoAODv9_UL16_postVFP.txt'
-with open(ttbar700to1000filename) as f:
-    ttbar700to1000files = [xrootdstr2 + s.strip() for s in f.readlines()]
+ul16ttbar700to1000filename = filedir + 'TT/TT_Mtt-700to1000_NanoAODv9_UL16' + VFP
+with open(ul16ttbar700to1000filename) as f:
+    ul16ttbar700to1000files = [xrootdstr2 + s.strip() for s in f.readlines()]
     
-ttbar1000toInffilename = filedir + 'TT/TT_Mtt-1000toInf_NanoAODv9_UL16_postVFP.txt'
-with open(ttbar1000toInffilename) as f:
-    ttbar1000toInffiles = [xrootdstr2 + s.strip() for s in f.readlines()]
+ul16ttbar1000toInffilename = filedir + 'TT/TT_Mtt-1000toInf_NanoAODv9_UL16' + VFP
+with open(ul16ttbar1000toInffilename) as f:
+    ul16ttbar1000toInffiles = [xrootdstr2 + s.strip() for s in f.readlines()]
     
-ttbarfiles = ttbar700to1000files + ttbar1000toInffiles # inclusion of both biased samples
+ul16ttbarfiles = ul16ttbar700to1000files + ul16ttbar1000toInffiles # inclusion of both biased samples
     
-ZprimeDMfilename = filedir + 'ZprimeDMToTTbar/ZprimeDMToTTbar_NanoAODv9_postVFP.txt'
-with open(ZprimeDMfilename) as f:
-    DM1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp1000" in s]
-with open(ZprimeDMfilename) as f:
-    DM1500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp1500" in s]
-with open(ZprimeDMfilename) as f:
-    DM2000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp2000" in s]
-with open(ZprimeDMfilename) as f:
-    DM2500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp2500" in s]
-with open(ZprimeDMfilename) as f:
-    DM3000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp3000" in s]
-with open(ZprimeDMfilename) as f:
-    DM3500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp3500" in s]
-with open(ZprimeDMfilename) as f:
-    DM4000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp4000" in s]
-with open(ZprimeDMfilename) as f:
-    DM4500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp4500" in s]
-with open(ZprimeDMfilename) as f:
-    DM5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp5000" in s]
+ul16ZprimeDMfilename = filedir + 'ZprimeDMToTTbar/ZprimeDMToTTbar_NanoAODv9_UL16' + VFP
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp1000" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM1500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp1500" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM2000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp2000" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM2500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp2500" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM3000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp3000" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM3500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp3500" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM4000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp4000" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM4500files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp4500" in s]
+with open(ul16ZprimeDMfilename) as f:
+    ul16DM5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "ResoIncl_MZp5000" in s]
 
-RSGluonfilename = filedir + 'RSGluonToTT/RSGluonToTT_NanoAODv9_UL16_postVFP.txt'
-with open(RSGluonfilename) as f:
-    RSGluon1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1000" in s]
-with open(RSGluonfilename) as f:
-    RSGluon1500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1500" in s]
-with open(RSGluonfilename) as f:
-    RSGluon2000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2000" in s]
-with open(RSGluonfilename) as f:
-    RSGluon2500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2500" in s]
-with open(RSGluonfilename) as f:
-    RSGluon3000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3000" in s]
-with open(RSGluonfilename) as f:
-    RSGluon3500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3500" in s]
-with open(RSGluonfilename) as f:
-    RSGluon4000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4000" in s]
-with open(RSGluonfilename) as f:
-    RSGluon4500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4500" in s]
-with open(RSGluonfilename) as f:
-    RSGluon5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-5000" in s]
+ul16RSGluonfilename = filedir + 'RSGluonToTT/RSGluonToTT_NanoAODv9_UL16' + VFP
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon1000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1000" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon1500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-1500" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon2000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2000" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon2500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-2500" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon3000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3000" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon3500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-3500" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon4000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4000" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon4500files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-4500" in s]
+with open(ul16RSGluonfilename) as f:
+    ul16RSGluon5000files = [xrootdstr2 + s.strip() for s in f.readlines() if "RSGluonToTT_M-5000" in s]
 
 datafilelist = os.listdir(filedir + 'JetHT/')
 for filename in datafilelist:
@@ -104,61 +105,56 @@ jetdatafiles = jetdatafiles2016 + jetdatafiles2017 + jetdatafiles2018 # All data
 """ Comment out whichever files you wish to not be included """
 
 filesets = {
-    # 'QCD':qcdfiles,
-    # 'DM1000':DM1000files,
-    # 'DM1500':DM1500files,
-    # 'DM2000':DM2000files,
-    # 'DM2500':DM2500files,
-    # 'DM3000':DM3000files,
-    # 'DM3500':DM3500files,
-    # 'DM4000':DM4000files,
-    # 'DM4500':DM4500files,
-    # 'DM5000':DM5000files,
-    # 'RSGluon1000':RSGluon1000files,
-    # 'RSGluon1500':RSGluon1500files,
-    # 'RSGluon2000':RSGluon2000files,
-    # 'RSGluon2500':RSGluon2500files,
-    # 'RSGluon3000':RSGluon3000files,
-    # 'RSGluon3500':RSGluon3500files,
-    # 'RSGluon4000':RSGluon4000files,
-    # 'RSGluon4500':RSGluon4500files,
-    # 'RSGluon5000':RSGluon5000files,
-    # 'TTbar':ttbarfiles,
-    # 'TTbar_biased_700to1000': ttbar700to1000files,
-    # 'TTbar_biased_1000toInf': ttbar1000toInffiles,
-    # 'JetHT':jetdatafiles,
+    'UL16_QCD':ul16qcdfiles,
+    'UL16_DM1000':ul16DM1000files,
+    'UL16_DM1500':ul16DM1500files,
+    'UL16_DM2000':ul16DM2000files,
+    'UL16_DM2500':ul16DM2500files,
+    'UL16_DM3000':ul16DM3000files,
+    'UL16_DM3500':ul16DM3500files,
+    'UL16_DM4000':ul16DM4000files,
+    'UL16_DM4500':ul16DM4500files,
+    'UL16_DM5000':ul16DM5000files,
+    'UL16_RSGluon1000':ul16RSGluon1000files,
+    'UL16_RSGluon1500':ul16RSGluon1500files,
+    'UL16_RSGluon2000':ul16RSGluon2000files,
+    'UL16_RSGluon2500':ul16RSGluon2500files,
+    'UL16_RSGluon3000':ul16RSGluon3000files,
+    'UL16_RSGluon3500':ul16RSGluon3500files,
+    'UL16_RSGluon4000':ul16RSGluon4000files,
+    'UL16_RSGluon4500':ul16RSGluon4500files,
+    'UL16_RSGluon5000':ul16RSGluon5000files,
+    'UL16_TTbar':ul16ttbarfiles,
     'JetHT2016_Data':jetdatafiles2016,
-    # 'JetHT2017_Data':jetdatafiles2017,
-    # 'JetHT2018_Data':jetdatafiles2018
+#     'JetHT2017_Data':jetdatafiles2017,
+#     'JetHT2018_Data':jetdatafiles2018
+#     'JetHT_Data':jetdatafiles, # all years
 }
 
-filesets_forweights = {
-    # 'QCD':qcdfiles,
-    # 'DM1000':DM1000files,
-    # 'DM1500':DM1500files,
-    # 'DM2000':DM2000files,
-    # 'DM2500':DM2500files,
-    # 'DM3000':DM3000files,
-    # 'DM3500':DM3500files,
-    # 'DM4000':DM4000files,
-    # 'DM4500':DM4500files,
-    # 'DM5000':DM5000files,
-    # 'RSGluon1000':RSGluon1000files,
-    # 'RSGluon1500':RSGluon1500files,
-    # 'RSGluon2000':RSGluon2000files,
-    # 'RSGluon2500':RSGluon2500files,
-    # 'RSGluon3000':RSGluon3000files,
-    # 'RSGluon3500':RSGluon3500files,
-    # 'RSGluon4000':RSGluon4000files,
-    # 'RSGluon4500':RSGluon4500files,
-    # 'RSGluon5000':RSGluon5000files,
-    # 'TTbar':ttbarfiles, # ttbarfiles to be subtracted from all years of JetHT data
-    'TTbar_2016':ttbarfiles, # ttbarfiles to be subtracted from JetHT2016 data
-    # 'TTbar_2017':ttbarfiles, # ttbarfiles to be subtracted from JetHT2017 data
-    # 'TTbar_2018':ttbarfiles, # ttbarfiles to be subtracted from JetHT2018 data
-    # 'JetHT':jetdatafiles, # all years
-    'JetHT2016_Data':jetdatafiles2016,
-    # 'JetHT2017_Data':jetdatafiles2017,
-    # 'JetHT2018_Data':jetdatafiles2018
-}
+# filesets_forweights = {
+#     'UL16_QCD':ul16qcdfiles,
+#     'UL16_DM1000':ul16DM1000files,
+#     'UL16_DM1500':ul16DM1500files,
+#     'UL16_DM2000':ul16DM2000files,
+#     'UL16_DM2500':ul16DM2500files,
+#     'UL16_DM3000':ul16DM3000files,
+#     'UL16_DM3500':ul16DM3500files,
+#     'UL16_DM4000':ul16DM4000files,
+#     'UL16_DM4500':ul16DM4500files,
+#     'UL16_DM5000':ul16DM5000files,
+#     'UL16_RSGluon1000':ul16RSGluon1000files,
+#     'UL16_RSGluon1500':ul16RSGluon1500files,
+#     'UL16_RSGluon2000':ul16RSGluon2000files,
+#     'UL16_RSGluon2500':ul16RSGluon2500files,
+#     'UL16_RSGluon3000':ul16RSGluon3000files,
+#     'UL16_RSGluon3500':ul16RSGluon3500files,
+#     'UL16_RSGluon4000':ul16RSGluon4000files,
+#     'UL16_RSGluon4500':ul16RSGluon4500files,
+#     'UL16_RSGluon5000':ul16RSGluon5000files,
+#     'UL16_TTbar':ul16ttbarfiles, # ttbarfiles to be subtracted from all years of JetHT data
+#     'JetHT2016_Data':jetdatafiles2016,
+#     'JetHT2017_Data':jetdatafiles2017,
+#     'JetHT2018_Data':jetdatafiles2018
+#     'JetHT':jetdatafiles, # all years
+# }
 
