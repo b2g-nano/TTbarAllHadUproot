f935a3a484a9721cae1aa147e61b8fad0ef1f2f0
7quantumphysics <acwillia@buffalo.edu>
2023-05-10 00:55:32 +0000

    Cleaner notebook; Ready to be turned into a simple script to run from terminal to get mistag rate plots and LUTs

--- HelpfulPlottingDefs.py
+++ HelpfulPlottingDefs.py
@@ -7,6 +7,7 @@ import mplhep as hep
 import matplotlib.pyplot as plt
 from hist.intervals import ratio_uncertainty
 
+
 def mkdir_p(mypath):
     '''Creates a directory. equivalent to using mkdir -p on the command line'''
 
@@ -147,3 +148,169 @@ def AnalysisLabels(Luminosity, axis):
               verticalalignment='bottom',
               transform=axis.transAxes
              )
+    
+def GetMistagInfo(JetHT, btag, year, VFP):
+    '''
+        JetHT --> dict, coffea output of jetht loaded from LoadData.py script
+        btag --> int, number of btags (0, 1, 2)
+        year --> int, run year        (2016, 2017, 2018)
+        VFP  --> str, APV or no APV   (preVFP, postVFP) 
+    '''
+    
+    if btag == 0:
+        cen = 6
+        fwd = 7
+    elif btag == 1:
+        cen = 8
+        fwd = 9
+    elif btag == 2:
+        cen = 10
+        fwd = 11
+    else:
+        print('No available number of btags selected')
+        return 0
+        
+    if year == 2016 and VFP == 'preVFP':
+        
+        JetHT_str = 'UL16preVFP_JetHTB_Data'
+        
+        Num_cen = JetHT['B_preVFP']['numerator'][JetHT_str, cen, :]
+        Denom_cen = JetHT['B_preVFP']['denominator'][JetHT_str, cen, :]
+        Num_fwd = JetHT['B_preVFP']['numerator'][JetHT_str, fwd, :]
+        Denom_fwd = JetHT['B_preVFP']['denominator'][JetHT_str, fwd, :]
+        
+        for Era in ['C', 'D', 'E', 'F']: #exclude B because histogram is initialized with B era
+            
+            JetHT_str = f'UL16preVFP_JetHT{Era}_Data'
+            
+            Num_cen += JetHT[Era+'_preVFP']['numerator'][JetHT_str, cen, :]
+            Denom_cen += JetHT[Era+'_preVFP']['denominator'][JetHT_str, cen, :]
+            Num_fwd += JetHT[Era+'_preVFP']['numerator'][JetHT_str, fwd, :]
+            Denom_fwd += JetHT[Era+'_preVFP']['denominator'][JetHT_str, fwd, :]
+            
+    elif year == 2016 and VFP == 'postVFP':
+        
+        JetHT_str = 'UL16postVFP_JetHTF_Data'
+        
+        Num_cen = JetHT['F_postVFP']['numerator'][JetHT_str, cen, :]
+        Denom_cen = JetHT['F_postVFP']['denominator'][JetHT_str, cen, :]
+        Num_fwd = JetHT['F_postVFP']['numerator'][JetHT_str, fwd, :]
+        Denom_fwd = JetHT['F_postVFP']['denominator'][JetHT_str, fwd, :]
+        
+        for Era in ['F', 'G', 'H']: #exclude F because histogram is initialized with F era
+            
+            JetHT_str = f'UL16postVFP_JetHT{Era}_Data'
+            
+            Num_cen += JetHT[Era+'_postVFP']['numerator'][JetHT_str, cen, :]
+            Denom_cen += JetHT[Era+'_postVFP']['denominator'][JetHT_str, cen, :]
+            Num_fwd += JetHT[Era+'_postVFP']['numerator'][JetHT_str, fwd, :]
+            Denom_fwd += JetHT[Era+'_postVFP']['denominator'][JetHT_str, fwd, :]
+            
+    Num_inc = Num_cen + Num_fwd
+    Denom_inc = Denom_cen + Denom_fwd
+
+    Output = {
+        'Num':   Num_inc,
+        'Denom': Denom_inc
+    }
+    
+    return Output
+    
+def GetMistagInfoCR(TTbar, JetHT, btag, year, VFP, scale): # CR --> "Contamination Removed"
+    '''
+        TTbar --> dict, coffea output of ttbar loaded from LoadMC.py script
+        JetHT --> dict, coffea output of jetht loaded from LoadData.py script
+        btag --> int, number of btags (0, 1, 2)
+        year --> int, run year        (2016, 2017, 2018)
+        VFP  --> str, APV or no APV   (preVFP, postVFP) 
+        scale --> list, collection of ttbar scale factors; first sf for 700-1000 and the other for 1000-Inf
+    '''
+    
+    str1 = '700_1000'
+    str2 = '1000_Inf'
+    stry = 'UL'+str(year-2000)
+    
+    if btag == 0:
+        cen = 6
+        fwd = 7
+    elif btag == 1:
+        cen = 8
+        fwd = 9
+    elif btag == 2:
+        cen = 10
+        fwd = 11
+    else:
+        print('No available number of btags selected')
+        return 0
+    
+    NumTT1cen = TTbar[f'{str1}_{VFP}']['numerator'][f'{stry}{VFP}_TTbar_{str1}', cen, :]
+    DenomTT1cen = TTbar[f'{str1}_{VFP}']['denominator'][f'UL16{VFP}_TTbar_{str1}', cen, :]
+    NumTT1fwd = TTbar[f'{str1}_{VFP}']['numerator'][f'UL16{VFP}_TTbar_{str1}', fwd, :]               
+    DenomTT1fwd = TTbar[f'{str1}_{VFP}']['denominator'][f'UL16{VFP}_TTbar_{str1}', fwd, :]
+
+    NumTT1_inc = NumTT1cen + NumTT1fwd
+    DenomTT1_inc = DenomTT1cen + DenomTT1fwd
+
+    NumTT2cen = TTbar[f'{str2}_{VFP}']['numerator'][f'{stry}{VFP}_TTbar_{str2}', cen, :]
+    DenomTT2cen = TTbar[f'{str2}_{VFP}']['denominator'][f'UL16{VFP}_TTbar_{str2}', cen, :]
+    NumTT2fwd = TTbar[f'{str2}_{VFP}']['numerator'][f'UL16{VFP}_TTbar_{str2}', fwd, :]               
+    DenomTT2fwd = TTbar[f'{str2}_{VFP}']['denominator'][f'UL16{VFP}_TTbar_{str2}', fwd, :]
+
+    NumTT2_inc = NumTT2cen + NumTT2fwd
+    DenomTT2_inc = DenomTT2cen + DenomTT2fwd
+
+    NumTT1_inc *= (-scale[0])
+    DenomTT1_inc *= (-scale[0])
+
+    NumTT2_inc *= (-scale[1])
+    DenomTT2_inc *= (-scale[1])
+
+    NumTT_inc = NumTT1_inc + NumTT2_inc
+    DenomTT_inc = DenomTT1_inc + DenomTT2_inc
+        
+    if year == 2016 and VFP == 'preVFP':
+        
+        JetHT_str = 'UL16preVFP_JetHTB_Data'
+        
+        Num_cen = JetHT['B_preVFP']['numerator'][JetHT_str, cen, :]
+        Denom_cen = JetHT['B_preVFP']['denominator'][JetHT_str, cen, :]
+        Num_fwd = JetHT['B_preVFP']['numerator'][JetHT_str, fwd, :]
+        Denom_fwd = JetHT['B_preVFP']['denominator'][JetHT_str, fwd, :]
+        
+        for Era in ['C', 'D', 'E', 'F']: #exclude B because histogram is initialized with B era
+            
+            JetHT_str = f'UL16preVFP_JetHT{Era}_Data'
+            
+            Num_cen += JetHT[Era+'_preVFP']['numerator'][JetHT_str, cen, :]
+            Denom_cen += JetHT[Era+'_preVFP']['denominator'][JetHT_str, cen, :]
+            Num_fwd += JetHT[Era+'_preVFP']['numerator'][JetHT_str, fwd, :]
+            Denom_fwd += JetHT[Era+'_preVFP']['denominator'][JetHT_str, fwd, :]
+            
+    elif year == 2016 and VFP == 'postVFP':
+        
+        JetHT_str = 'UL16postVFP_JetHTF_Data'
+        
+        Num_cen = JetHT['F_postVFP']['numerator'][JetHT_str, cen, :]
+        Denom_cen = JetHT['F_postVFP']['denominator'][JetHT_str, cen, :]
+        Num_fwd = JetHT['F_postVFP']['numerator'][JetHT_str, fwd, :]
+        Denom_fwd = JetHT['F_postVFP']['denominator'][JetHT_str, fwd, :]
+        
+        for Era in ['F', 'G', 'H']: #exclude F because histogram is initialized with F era
+            
+            JetHT_str = f'UL16postVFP_JetHT{Era}_Data'
+            
+            Num_cen += JetHT[Era+'_postVFP']['numerator'][JetHT_str, cen, :]
+            Denom_cen += JetHT[Era+'_postVFP']['denominator'][JetHT_str, cen, :]
+            Num_fwd += JetHT[Era+'_postVFP']['numerator'][JetHT_str, fwd, :]
+            Denom_fwd += JetHT[Era+'_postVFP']['denominator'][JetHT_str, fwd, :]
+            
+    Num_inc = Num_cen + Num_fwd + NumTT_inc
+    Denom_inc = Denom_cen + Denom_fwd + DenomTT_inc
+
+    Output = {
+        'Num':   Num_inc,
+        'Denom': Denom_inc
+    }
+    
+    return Output
+        
